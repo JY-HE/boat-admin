95677811d96ab9c9b0b99210c4659f38fa7ee546
何嘉泳 <hejiayong@genew.com>
2022-09-15 10:02:09 +0800

    初始化项目

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    // parser 代码解析依赖
+    parser: 'vue-eslint-parser',
+
+    parserOptions: {
+        // 替换原有的代码解析器
+        parser: '@typescript-eslint/parser',
+        // 声明 es 版本
+        ecmaVersion: 2020,
+        // 声明源码类型
+        sourceType: 'module',
+        // 其他语言扩展，包含jsx，全局严格模式等
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+
+    // 继承插件的规则配置
+    extends: [
+        'plugin:vue/vue3-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+        'plugin:prettier/recommended',
+        './.eslintrc-auto-import.json',
+    ],
+
+    // 自定义规则配置
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+    },
+};
---
+++ src/views/Home.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="home">
+        <div>
+            <a href="https://vitejs.dev" target="_blank">
+                <img src="/vite.svg" class="logo" alt="Vite logo" />
+            </a>
+            <a href="https://vuejs.org/" target="_blank">
+                <img src="@/assets/vue.svg" class="logo vue" alt="Vue logo" />
+            </a>
+            <a href="https://pinia.web3doc.top/" target="_blank">
+                <img src="https://pinia.web3doc.top/logo.svg" class="logo pinia" alt="Pinia logo" />
+            </a>
+        </div>
+        <el-button type="success" round>按钮</el-button>
+        <HelloWorld msg="Vite + Vue + Pinia" />
+        <UsePinia />
+        <Request />
+    </div>
+</template>
+
+<script lang="ts" setup>
+import HelloWorld from '@/components/HelloWorld.vue';
+import UsePinia from '@/components/UsePinia.vue';
+import Request from '@/components/Request.vue';
+</script>
+<style lang="scss">
+.home {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    .logo {
+        height: 6em;
+        padding: 1.5em;
+        will-change: filter;
+    }
+
+    .logo:hover {
+        filter: drop-shadow(0 0 2em #646cffaa);
+    }
+
+    .logo.vue:hover {
+        filter: drop-shadow(0 0 2em #42b883aa);
+    }
+
+    .logo.pinia:hover {
+        filter: drop-shadow(0 0 2em rgb(245, 208, 88));
+    }
+}
+</style>

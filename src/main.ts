21acd0a6bc2ffcd359ffd3adb2efc521113c6472
何嘉泳 <hejiayong@genew.com>
2022-09-17 17:01:35 +0800

    feat：新建css文件夹

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import '@/assets/public.css'
+import '@/assets/css/public.css'
 import App from './App.vue'
 import store from './store'
 import router from '@/router';

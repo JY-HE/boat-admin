7cbbecc0a83d047e1cbc216f95eb216fba96e58b
何嘉泳 <hejiayong@genew.com>
2024-05-20 15:02:13 +0800

    feat：更换图片

--- src/views/componentsView/components/PicturePreview.vue
+++ src/views/componentsView/components/PicturePreview.vue
@@ -23,7 +23,7 @@
 <script setup lang="ts">
 import exampleImage from '@/assets/img/7.png'; // 使用 import 语句加载图片
 
-const img1Src = ref('/public/img/截图20240518104550.png'); // 路径相对于 public 目录
+const img1Src = ref('/public/img/8.jpg'); // 路径相对于 public 目录
 const img2Src = ref(exampleImage);
 const previewSrc = ref('');
 const isOriginalSize = ref(false);

eebc0fcfaa487f62528677794edfdaed45b8453d
何嘉泳 <hejiayong@genew.com>
2024-10-25 16:26:09 +0800

    feat：新增堆叠面积图

--- src/views/eChartsView/components/line/echartsData.ts
+++ src/views/eChartsView/components/line/echartsData.ts
@@ -1,5 +1,6 @@
 import { DefineComponent } from 'vue';
 import BasicLine from './components/BasicLine.vue';
+import StackedAreaChart from './components/StackedAreaChart.vue';
 
 interface EChartsData {
     title: string;
@@ -16,5 +17,12 @@ export default [
         componentChart: BasicLine,
         darkSrc: 'https://s21.ax1x.com/2024/10/21/pAaofm9.png',
         lightSrc: 'https://s21.ax1x.com/2024/10/21/pAao4T1.png',
+    },
+    {
+        title: '堆叠面积图',
+        enTitle: 'Stacked Area Chart',
+        componentChart: StackedAreaChart,
+        darkSrc: 'https://s21.ax1x.com/2024/10/25/pAw0ytA.png',
+        lightSrc: 'https://s21.ax1x.com/2024/10/25/pAw0dOO.png',
     },
 ] as EChartsData[];

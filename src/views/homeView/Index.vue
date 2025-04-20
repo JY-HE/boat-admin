0923b613f76ded7c96f5c78060db483098a0f81b
何嘉泳 <893404614@qq.com>
2025-04-20 18:57:42 +0800

    feat：开发home

--- src/views/homeView/Index.vue
+++ src/views/homeView/Index.vue
@@ -1,8 +1,9 @@
 <template>
     <div>
-        <div class="p-4 flex justify-start items-start flex-col scrollbarStyle gap-4">
+        <div class="HomeView p-4 flex justify-start items-start flex-col scrollbarStyle gap-4">
             <StatisticInfo />
             <StatisticCards />
+            <Dynamics />
         </div>
     </div>
 </template>
@@ -10,6 +11,14 @@
 <script lang="ts" setup>
 import StatisticInfo from './components/StatisticInfo.vue';
 import StatisticCards from './components/StatisticCards.vue';
+import Dynamics from './components/Dynamics.vue';
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.HomeView {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-auto-rows: pxToRem(126);
+    gap: pxToRem(16);
+}
+</style>
---
+++ src/views/homeView/components/Dynamics.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="Dynamics h-full rounded-xl"></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss">
+.Dynamics {
+    grid-area: 3 / 3 / 6 / 5;
+    @include panelStyle;
+    @include panelShadow(0.25);
+}
+</style>

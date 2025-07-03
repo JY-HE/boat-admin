559027da573adefef71caab1e7152c0caebb37b6
何嘉泳 <hejiayong@genew.com>
2025-07-03 18:10:59 +0800

    feat：添加图片卡片

---
+++ src/views/templatesView/components/cards/ImageCard.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="ImageCard">
+        <div class="image-box">
+            <img :src="image" alt="Image" />
+        </div>
+        <div class="image-info">
+            <div class="tag">{{ tag }}</div>
+            <div class="title font-style-2 text-h2 mb-4">
+                {{ title }}
+            </div>
+            <div class="flex justify-start align-center gap-4 font-style-4 text-h3">
+                <div>
+                    <BoatIconfont icon="&#xe627;" />
+                    <span class="ml-1">{{ formatNumberWithCommas(views) }}</span>
+                </div>
+                <div>
+                    <BoatIconfont icon="&#xe6af;" />
+                    <span class="ml-1">{{ formatNumberWithCommas(reviews) }}</span>
+                </div>
+                <div>{{ date }}</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { formatNumberWithCommas } from '@/utils/global';
+
+interface Props {
+    image?: string;
+    tag?: string;
+    title?: string;
+    views?: number;
+    reviews?: number;
+    date?: string;
+}
+
+defineProps<Props>();
+</script>
+
+<style lang="scss">
+.ImageCard {
+    width: pxToRem(400);
+    @include flexCenter(flex-start, flex-start, true);
+    @include panelStyle;
+    @include panelShadow(0.25);
+    border-radius: pxToRem(12);
+    overflow: hidden;
+
+    .image-box {
+        @include whrem(100%, 244);
+
+        img {
+            @include wh;
+            object-fit: fill;
+        }
+    }
+
+    .image-info {
+        @include whrem(100%, 146);
+        padding: pxToRem(16);
+        @include flexCenter(flex-start, flex-start, true);
+
+        .tag {
+            @include fontStyle(4);
+            @include fontColor(2);
+            padding: pxToRem(2) pxToRem(4);
+            border-radius: pxToRem(4);
+            @include disabledColor(0.15, background-color);
+            margin-bottom: pxToRem(8);
+        }
+
+        .title {
+            display: -webkit-box;
+            -webkit-box-orient: vertical;
+            -webkit-line-clamp: 2;
+            overflow: hidden;
+            text-overflow: ellipsis;
+        }
+    }
+}
+@include darkStyle {
+    .ImageCard {
+        .image-info {
+            .tag {
+                @include whiteColor(1, color);
+            }
+        }
+    }
+}
+</style>
--- src/views/templatesView/components/cards/Index.vue
+++ src/views/templatesView/components/cards/Index.vue
@@ -30,12 +30,29 @@
                 />
             </div>
         </div>
+        <div class="modules">
+            <BoatTitle class="flex-1" title="图片卡片" />
+            <div class="w-full flex gap-6">
+                <ImageCard
+                    class="flex-1"
+                    v-for="(item, index) in imageCardData"
+                    :key="index"
+                    :image="item.image"
+                    :tag="item.tag"
+                    :title="item.title"
+                    :views="item.views"
+                    :reviews="item.reviews"
+                    :date="item.date"
+                />
+            </div>
+        </div>
     </div>
 </template>
 
 <script lang="ts" setup>
 import DigitalScrolling from './DigitalScrolling.vue';
 import ProgressCard from './ProgressCard.vue';
+import ImageCard from './ImageCard.vue';
 
 const digitalScrollingData = [
     {
@@ -96,6 +113,41 @@ const progressCardData = [
         iconBackgroundColor: '#f7ba1e',
         icon: '&#xeb95;',
         progressColor: '#409eff',
+    },
+];
+
+const imageCardData = [
+    {
+        image: 'https://picsum.photos/400/240?1',
+        tag: '热门',
+        title: 'AI技术在医疗领域的创新应用与发展前景',
+        views: 551245,
+        reviews: 555,
+        date: '1月19日 周一',
+    },
+    {
+        image: 'https://picsum.photos/400/240?2',
+        tag: '科技',
+        title: 'AI技术在医疗领域的创新应用与发展前景',
+        views: 12478,
+        reviews: 1462,
+        date: '5月9日 周二',
+    },
+    {
+        image: 'https://picsum.photos/400/240?3',
+        tag: '技术',
+        title: 'AI技术在医疗领域的创新应用与发展前景',
+        views: 12155,
+        reviews: 7869,
+        date: '11月29日 周一',
+    },
+    {
+        image: 'https://picsum.photos/400/240?4',
+        tag: '云计算',
+        title: 'AI技术在医疗领域的创新应用与发展前景',
+        views: 45746,
+        reviews: 55125,
+        date: '5月18日 周三',
     },
 ];
 </script>
